66419286f45d01b95df0df42f59bc56ee5bbafe1
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-02-05 02:55:27 +0530

    updating the inital ui

--- components/Header.js
+++ components/Header.js
@@ -1,48 +1,66 @@
 import React from "react";
 import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
-      <Button
-        color="gray"
-        buttonType="outline"
-        rounded={true}
-        iconOnly={true}
-        ripple="dark"
-        className="h-20 w-20 border-0"
+    <header className="sticky top-0 z-50 flex items-center px-4 justify-between max-w-screen bg-white ">
+      <div className="flex items-center space-x-1">
+        <Button
+          color="gray"
+          buttonType="outline"
+          rounded={true}
+          iconOnly={true}
+          ripple="dark"
+          className="border-0"
+        >
+          <Icon name="menu" size="2xl" />
+        </Button>
+
+        <Icon name="description" size="4xl" color="blue" />
+        <h1 className="hidden md:inline-flex text-gray-700 text-2xl self-end">
+          Docs
+        </h1>
+      </div>
+
+      <div
+        className="relative flex items-center px-3 sm:px-5 py-2  bg-[#F1F3F4] text-gray-600 rounded-md focus-within:shadow-default focus-within:bg-white w-[200px] max-w-2xl md:flex-grow m-2 overflow-hidden"
       >
-        <Icon name="menu" size="3xl" />
-      </Button>
-
-      <Icon name="description" size="5xl" color="blue" />
-      <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
-
-      <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-md focus-within:shadow-default focus-within:bg-white">
-        <Icon name="search" size="3xl" color="darkgray" />
+        <Icon
+          name="search"
+          size="2xl"
+          color="darkgray"
+          
+        />
         <input
-          className="outline-none bg-transparent px-5 text-base flex-grow"
+          className="outline-none flex-shrink bg-transparent ml-3 text-base w-full"
           type="text"
           placeholder="Search"
         />
-      </div>
-      <Button
-        color="gray"
-        buttonType="outline"
-        rounded={true}
-        iconOnly={true}
-        ripple="dark"
-        className="ml-5 md:ml-20 h-20 w-20 border-0"
-      >
-        <Icon name="apps" size="3xl" color="gray" />
-      </Button>
+      </div>  
 
-      <img
-        loading="lazy"
-        className="cursor-pointer h-12 w-12 rounded-full ml-2"
-      />
-    </div>
+      <div className="flex items-center space-x-1">
+        <Button
+          color="gray"
+          buttonType="outline"
+          rounded={true}
+          iconOnly={true}
+          ripple="dark"
+          className="h-18 w-18 border-0"
+        >
+          <Icon name="apps" size="3xl" color="gray" />
+        </Button>
+
+        <div className="relative cursor-pointer h-10 w-10 rounded-full">
+          <Image
+            className="rounded-full"
+            layout="fill"
+            src="https://rb.gy/en5awm"
+          />
+        </div>
+      </div>
+    </header>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import "@material-tailwind/react/tailwind.css";
+// import "tailwindcss/tailwindcss.css";
+import "tailwindcss/tailwind.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
@@ -10,6 +12,10 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
       </Head>
       <Component {...pageProps} />
     </>
