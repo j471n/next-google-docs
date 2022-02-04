5e1f384268548780b56bb9f64fab9683b6b34ba5
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-02-05 01:30:58 +0530

    Adding the blank document div

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Icon from "@material-tailwind/react/Icon";
+import Button from "@material-tailwind/react/Button";
 
 export default function Home() {
   return (
@@ -11,8 +13,44 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      Google docs
 
+      <section className="bg-[#f8f9fa] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6 ">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+
+            <Button
+              color="gray"
+              buttonType="outline"
+              rounded={true}
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="2xl" />
+            </Button>
+          </div>
+
+          <div>
+            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+              <Image src="https://rb.gy/wlvbum" layout="fill" />
+            </div>
+            <p className="mt-2 text-gray-700 text-sm">Blank</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Bottom Container */}
+
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-grow">My Document</h2>
+            <p className="mr-12">Date Created</p>
+            <Icon name="folder" size="2xl" color="gray" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
