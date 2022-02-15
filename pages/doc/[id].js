0996066ec78f4c7ba2c10d802a05bdfc95de5c87
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-02-15 12:15:50 +0530

    Update [id].js

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -12,6 +12,7 @@ import {
   getProviders,
 } from "next-auth/client";
 import Login from "../../components/Login";
+import Head from "next/head";
 
 export default function Doc({ id, providers }) {
   const [session] = useSession();
@@ -32,76 +33,91 @@ export default function Doc({ id, providers }) {
   }
 
   return (
-    <div>
-      <header className="flex justify-between items-center p-3 pb-1">
-        <span
-          onClick={() => router.push("/")}
-          className="cursor-pointer relative group"
-        >
-          <Icon name="description" size="5xl" color="blue" />
-
-          <Button
-            color="gray"
-            buttonType="filled"
-            iconOnly={true}
-            rounded={true}
-            size="lg"
-            className="hidden print:hidden absolute top-0 sm:inline-flex opacity-0 group-hover:opacity-100 z-50"
-            ripple="light"
+    <>
+      <Head>
+        <title>{`${snapshot?.data()?.fileName} - Docs`}</title>
+      </Head>
+      <div>
+        <header className="flex justify-between items-center p-3 pb-1">
+          <span
+            onClick={() => router.push("/")}
+            className="cursor-pointer relative group"
           >
-            <Icon name="arrow_back" size="xl" color="white" />
-          </Button>
-        </span>
+            <Icon name="description" size="5xl" color="blue" />
 
-        <div className="flex-grow px-2">
-          <h2 className="text-lg font-semibold capitalize">
-            {snapshot?.data()?.fileName || "---"}
-          </h2>
+            <Button
+              color="gray"
+              buttonType="filled"
+              iconOnly={true}
+              rounded={true}
+              size="lg"
+              className="hidden print:hidden absolute left-0 top-0 sm:inline-flex opacity-0 group-hover:opacity-100 z-50"
+              ripple="light"
+            >
+              <Icon name="arrow_back" size="xl" color="white" />
+            </Button>
+          </span>
+          {/* file_download */}
+          <div className="flex-grow px-2">
+            <h2 className="text-lg font-semibold capitalize">
+              {snapshot?.data()?.fileName}
+            </h2>
 
-          <div className="flex items-center text-sm space-x-2 text-gray-600 print:hidden">
-            <p className="option">File</p>
-            <p className="option">Edit</p>
-            <p className="option">View</p>
-            <p className="option">Insert</p>
-            <p className="option">Format</p>
-            <p className="option">Tools</p>
+            <div className="flex items-center text-sm space-x-2 text-gray-600 print:hidden">
+              <p className="option">File</p>
+              <p className="option">Edit</p>
+              <p className="option">View</p>
+              <p className="option">Insert</p>
+              <p className="option">Format</p>
+              <p className="option">Tools</p>
+            </div>
           </div>
-        </div>
+          <div className="flex items-center space-x-2">
+            <Button
+              color="blue"
+              buttonType="filled"
+              className="hidden md:inline-flex print:hidden"
+              ripple="light"
+            >
+              <Icon name="people" size="md" /> Share
+            </Button>
+            <Button
+              buttonType="filled"
+              size="regular"
+              iconOnly={true}
+              onClick={() => window?.print()}
+              ripple="dark"
+              className="border-0 bg-black hover:bg-gray-700 print:hidden"
+            >
+              <Icon name="print" size="2xl" />
+            </Button>
 
-        <div className="flex items-center space-x-2">
-          <Button
-            color="blue"
-            buttonType="filled"
-            className="hidden md:inline-flex print:hidden"
-            ripple="light"
-          >
-            <Icon name="people" size="md" /> Share
-          </Button>
-          <Button
-            buttonType="filled"
-            size="regular"
-            iconOnly={true}
-            onClick={()=> window?.print()}
-            ripple="dark"
-            className="border-0 bg-black hover:bg-gray-700 print:hidden"
-          >
-            <Icon name="print" size="2xl" />
-          </Button>
+            {/* <Button
+              buttonType="filled"
+              size="regular"
+              iconOnly={true}
+              onClick={generatePDF}
+              ripple="dark"
+              className="border-0 bg-black hover:bg-gray-700 print:hidden"
+            >
+              <Icon name="file_download" size="2xl" />
+            </Button> */}
 
-          <div className="relative print:hidden h-10 w-10 rounded-full cursor-pointer">
-            <Image
-              onClick={signOut}
-              src={session?.user?.image}
-              layout="fill"
-              className="rounded-full"
-              alt="profile"
-            />
+            <div className="relative print:hidden h-10 w-10 rounded-full cursor-pointer">
+              <Image
+                onClick={signOut}
+                src={session?.user?.image}
+                layout="fill"
+                className="rounded-full"
+                alt="profile"
+              />
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      <TextEditor snapshot={snapshot} />
-    </div>
+        <TextEditor snapshot={snapshot} />
+      </div>
+    </>
   );
 }
 
